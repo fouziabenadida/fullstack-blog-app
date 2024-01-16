ce57250d22ad857b98c32b115a934ffae608202e
benadidafouzia <benadidafouzia@gmail.com>
2024-01-16 17:10:45 +0300

    refactor

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,24 +1,28 @@
+const express = require("express")
+const bcrypt = require('bcrypt');
+const User = require('../models/auth')
 
 
 const register = async (req, res) => {
-    try {
-        const existingUser = await User.findOne({ $or: [{ email: req.body.email }, { username: req.body.username }] });
-        if (existingUser) {
-            return res.status(409).json("User already exists!");
-        }
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-        });
+    const { username, email, password } = req.body;
 
+    try {
+
+        if (!username || !password || !email) {
+            return res.status(400).json({ error: 'Username and email password are required' });
+        }
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username or email already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
 
-        res.status(200).json("User has been created.", { newUser });
-    }
-    catch (e) {
-        console.log(e);
-        res.status(400).json(e);
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Error in registration:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,21 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 const Register = () => {
   const [inputs, setInputs] = useState({
     username: "",
     email: "",
     password: "",
   });
+  const [err, setError] = useState(null);
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/register",
+        JSON.stringify(inputs),
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.status === 200 || response.status === 201) {
+        console.log("User registered successfully!");
+        // toast.success('Registration successful!', {
+        //   position: toast.POSITION.TOP_RIGHT,
+        // });
+        setInputs({
+          username: "",
+          email: "",
+          password: "",
+        });
+      } else {
+        console.error("Registration failed");
+        // toast.error('Registration failed. Please try again.', {
+        //   position: toast.POSITION.TOP_RIGHT,
+        // });
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+    }
+  };
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
         <input
+          value={inputs.username}
           required
           type="text"
           placeholder="username"
@@ -24,6 +60,7 @@ const Register = () => {
         />
         <input
           required
+          value={inputs.email}
           type="email"
           placeholder="email"
           name="email"
@@ -32,11 +69,12 @@ const Register = () => {
         <input
           required
           type="password"
+          value={inputs.password}
           placeholder="password"
           name="password"
           onChange={handleChange}
         />
-        <button>Register</button>
+        <button onClick={handleSubmit}>Register</button>
         <p>This is an error!</p>
         <span>
           Don't you have an account <Link to="/login">Login</Link>
